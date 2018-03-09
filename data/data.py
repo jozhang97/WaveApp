500bafab14267ca22dc41bd3d444f230d9e913f6
JEFFREY ZHANG <jozhang@berkeley.edu>
2018-03-08 20:22:07 -0800

    Defining data specific processing mechanism

---
+++ data/data.py
@@ -0,0 +1,9 @@
+class Data(object):
+  def preprocess_input(self):
+    pass
+
+  def preproces_target(self):
+    pass
+
+  def postprocess_target(self):
+    pass
---
+++ data/yesno.py
@@ -0,0 +1,5 @@
+from data import Data
+
+def YESNO(Data):
+  def preprocess_obs(self):
+    pass
